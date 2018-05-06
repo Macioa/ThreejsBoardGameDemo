7533c97c5831b07670430c2d8b1319081e55b16b
DESKTOP-J21VMC2\ryanm <asdf@asdf.com>
2018-05-06 10:56:05 -0500

    Instancing renderer did not work as hoped. Rewrote to work more like the example.

--- Main.js
+++ Main.js
@@ -1,55 +1,50 @@
 //js/three.min.js
 //js/OrbitControls.js
 
-class Main {
-	constructor(){
-		console.log("Constructing game instance..");
-		
-		//create camera, scene, renderer, and controls
-		var camera, scene, renderer, controls;
-		
-		this.camera = new THREE.PerspectiveCamera( 70, window.innderWidth / window.innerHeight, 0.01, 10 );
-		this.camera.position.z = 1;
-		this.scene = new THREE.Scene();
-		
-		this.renderer = new THREE.WebGLRenderer( { antialias: true } );
-		this.renderer.setSize( window.innerWidth, window.innerHeight );
-		document.body.appendChild( this.renderer.domElement );
-		
-		this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
-		
-		
-		//update render on window resize
-		window.addEventListener('resize', function() {
-			this.width = window.innerWidth;
-			this.height = window.innerHeight;
-			this.renderer.setSize(this.width, this.height);
-			this.camera.aspect = this.width/this.height;
-			this.camera.updateProjectionMatrix();
-		} );
-		
-		// add additional listeners (mouse down & scroll are used by OrbitControls)
-	//	document.addEventListener('dblclick', this.dblClick, false);
-	//	document.addEventListener('mousedown' this.mouseDown, false);
-	
-		console.log("Game instance constructed");
-		this.animate();
-	}
-	/*
-	mouseDown(event) {
-		event.preventDefault();
-		alert("Mouse Down");
-	}
-	
-	dblClick(event) {
-		event.preventDefault();
-		alert("Double Clicked");
-	}*/
-	function animate() {
-		requestAnimationFrame( animate );
-		this.renderer.render( this.scene, this.camera);
-	}
-	
+var camera, scene, renderer;
+var geometry, material, mesh;
+var controls;
+ 
+init();
+animate();
+
+window.addEventListener('resize', function() {
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+	renderer.setSize(width, height);
+	camera.aspect = width/height;
+	camera.updateProjectionMatrix();
+	} );
+
+ 
+function init() {
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+    camera.position.z = 1;
+ 
+    scene = new THREE.Scene();
+ 
+    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+    material = new THREE.MeshNormalMaterial();
+ 
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+ 
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+	document.addEventListener('dblclick', onMouseDown, false);
+
+	controls = new THREE.OrbitControls( camera, renderer.domElement );
+}
+ 
+function animate() { 
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+	console.log("animating");
 }
 
-var instance = new Main();
+function onMouseDown(event) {
+	event.preventDefault();
+}
+
+
